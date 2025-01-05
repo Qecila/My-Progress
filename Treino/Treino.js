68b5fcfc14130857e13014bf3bf39db803b36322
Qecila <alice_queiroz@msn.com>
2025-01-05 05:38:55 +0000

    Removendo o arquivo desnecessário

--- Treino/Treino.js
+++
@@ -1,6 +0,0 @@
-let userName = getFirstName("Alice Silva Queiroz")
-console.log("Seja bem vinda " + userName)
-function getFirstName(name){
-    let firstName = name.split(" ")[0]
-    return firstName
-}
