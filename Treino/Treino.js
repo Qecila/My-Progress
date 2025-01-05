755a1fb2c39034019b987aa3bcaad8964015501a
Qecila <alice_queiroz@msn.com>
2025-01-05 05:37:03 +0000

    Update Treino.js

--- Treino/Treino.js
+++ Treino/Treino.js
@@ -1,6 +1,1 @@
-let userName = getFirstName("Alice Silva Queiroz")
-console.log("Seja bem vinda " + userName)
-function getFirstName(name){
-    let firstName = name.split(" ")[0]
-    return firstName
-}
+
